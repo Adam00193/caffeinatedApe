b2cc40a702f00f91922ea9b4fe12e19000300f2b
ccoder23 <pettadam347@gmail.com>
2016-11-17 16:27:20 -0700

    added menu

--- menu.php
+++ menu.php
@@ -12,19 +12,19 @@
     </style>
 </head>
 <body>
-<div class="container">
-    <h1>Menu</h1>
+    <div class="container">
+        <h1>Menu</h1>
 
-    <h3>Fresh Brew-$1.50</h3>
-    <h3>Americano-$2.00</h3>
-    <h3>Hot Chocolate-$2.00</h3>
-    <h3>Chai Tea-$3.50</h3>
-    <h3>Latte-$3.50</h3>
-    <h3>Mocha-$3.50</h3>
-    <h3>Cappuccino-$3.50</h3>
-    <h3>Macchiato-$3.50</h3>
-    <h3>Pumpkin Spice-$3.50</h3>
-    <h3>Soy Milk-¢.50 Each</h3>
-</div>
+        <h3>Fresh Brew-$1.50</h3>
+        <h3>Americano-$2.00</h3>
+        <h3>Hot Chocolate-$2.00</h3>
+        <h3>Chai Tea-$3.50</h3>
+        <h3>Latte-$3.50</h3>
+        <h3>Mocha-$3.50</h3>
+        <h3>Cappuccino-$3.50</h3>
+        <h3>Macchiato-$3.50</h3>
+        <h3>Pumpkin Spice-$3.50</h3>
+        <h3>Soy Milk-¢.50 Each</h3>
+    </div>
 </body>
 </html>
