b874ed6f61e7b525a14c62790a3c53986b5222cc
ccoder23 <pettadam347@gmail.com>
2017-01-26 16:26:07 -0700

    Revert "completed the front page"
    This reverts commit f755bb0f19e6d75d6fd4cc3a2ae62a6474b01244.
    
    # Conflicts:
    #	index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
   <div class="site-wrapper">
     <div class="site-wrapper-inner">
       <div class="cover-container">
+<<<<<<< HEAD
         <div class="inner cover">
           <div class=cover-container>
             <div class="masthead clearfix">
@@ -26,6 +27,21 @@
           <h1 class="cover-heading">Welcome</h1>
           <img class="jtimg" src="lib/imgs/caffeinatedApe.png" alt="">
           <p class="lead">Caffeinated is located in sunny Shiprock, New Mexico at the junction.  Take a look around, like some stuff.  Enjoy.  :)</p>
+=======
+        <div class="masthead clearfix">
+          <div class="inner">
+            <h3 class="masthead-brand">Cover</h3>
+            <nav class="nav nav-masthead">
+              <a class="nav-link active" href="#">Home</a>
+              <a class="nav-link" href="#">Features</a>
+              <a class="nav-link" href="#">Contact</a>
+            </nav>
+          </div>
+        </div>
+        <div class="inner cover">
+          <h1 class="cover-heading">Cover your page.</h1>
+          <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+>>>>>>> parent of f755bb0... completed the front page
           <p class="lead">
             <a href="#" class="btn btn-lg btn-secondary">Learn more</a>
           </p>
@@ -39,5 +55,22 @@
     </div>
   </div>
 
+<<<<<<< HEAD
+=======
+  <div class="container" id="container">
+    <div class="jumbotron">
+      <div class="col-md-3">
+        <img class="jtimg" alt="IMAGE UNAVAILABLE" src="lib/imgs/caffeinatedApe.png">
+      </div>
+      <div class="col-md-9">
+        <h1 class="jth1">Caffeinated Ape</h1>
+        <br>
+        <p class="jtp">Mobile Cafe Shop</p>
+        <a class="btn btn-primary" id="jtbtn" href="#" role="button">Learn more</a>
+      </div>
+    </div>
+    <h1 class="bh1"></h1>
+  </div>
+>>>>>>> parent of f755bb0... completed the front page
 </body>
 </html>
